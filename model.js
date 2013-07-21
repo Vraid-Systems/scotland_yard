5ab57bb90a32bda270b723572059ae30458f4567
Jason Zerbe <jzerbe@vraidsys.com>
2013-07-21 16:27:50 -0600

    organize by logical unit

--- model.js
+++ model.js
@@ -1,31 +1,13 @@
 // data model loaded on client and server
 
-Meteor.Collection.prototype.putNodes = function(theNodeMap) {
-    for (var theNode in theNodeMap) {
-        theNeighbors = theNodeMap[theNode];
+// information about ongoing games
+Games = new Meteor.Collection("games");
+Players = new Meteor.Collection("players");
 
-        var existingValue = this.findOne({ node: { $all: [theNode] } });
-        if (existingValue) {
-            this.update(
-                    {
-                        node: {$all: [theNode]}
-                    },
-                    {
-                        $set: {neighbors: theNeighbors}
-                    }
-            );
-        } else {
-            this.insert(
-                    {
-                        node: theNode,
-                        neighbors: theNeighbors
-                    }
-            );
-        }
-    }
-};
 
-Positions = new Meteor.Collection("positions"); // map board position # to screen x,y
+// map board position # to screen x,y
+Positions = new Meteor.Collection("positions");
+
 
 // maps of valid moves by various transportation means
 Underground = new Meteor.Collection("underground");
@@ -33,5 +15,18 @@ Bus = new Meteor.Collection("bus");
 Taxi = new Meteor.Collection("taxi");
 Black = new Meteor.Collection("black"); // Mr. X only - river transit or any of above
 
-Players = new Meteor.Collection("players");
-Games = new Meteor.Collection('games');
+Meteor.Collection.prototype.putNodes = function(theNodeMap) {
+    for (var theNode in theNodeMap) {
+        theNeighbors = theNodeMap[theNode];
+
+        var existingValue = this.findOne( { node: { $all: [theNode] } } );
+        if (existingValue) {
+            this.update(
+                    { node: {$all: [theNode]} },
+                    { $set: {neighbors: theNeighbors} }
+            );
+        } else {
+            this.insert( { node: theNode, neighbors: theNeighbors } );
+        }
+    }
+};
