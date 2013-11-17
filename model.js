d61a755517c502e37ca05159aae20c13f60463cb
Jason Zerbe <jzerbe@vraidsys.com>
2013-11-17 15:44:55 -0700

    add client CRUD allowanced for Player self manipulation

--- model.js
+++ model.js
@@ -1,22 +1,29 @@
 // data model loaded on client and server
+function createStdCollectionLink(collectionName, collectionObj) {
+    if (Meteor.isServer) {
+        Meteor.publish(collectionName, function () {
+            return collectionObj.find();
+        });
+    }
+    if (Meteor.isClient) {
+        Meteor.subscribe(collectionName);
+    }
+}
 
 // map board position # to screen x,y
 Positions = new Meteor.Collection("positions");
-if (Meteor.isServer) {
-    Meteor.publish("positions", function () {
-        return Positions.find();
-    });
-}
-if (Meteor.isClient) {
-    Meteor.subscribe("positions");
-}
+createStdCollectionLink("positions", Positions);
 
 // maps of valid moves by various transportation means
 Underground = new Meteor.Collection("underground");
+createStdCollectionLink("underground", Underground);
 Bus = new Meteor.Collection("bus");
+createStdCollectionLink("bus", Bus);
 Taxi = new Meteor.Collection("taxi");
+createStdCollectionLink("taxi", Taxi);
 Black = new Meteor.Collection("black"); // Mr. X only - river transit or any of above
-
+createStdCollectionLink("black", Black);
+// used in server-side initialization of transport collections
 Meteor.Collection.prototype.putNodes = function(theNodeMap) {
     for (var theNode in theNodeMap) {
         theNeighbors = theNodeMap[theNode];
@@ -33,5 +40,17 @@ Meteor.Collection.prototype.putNodes = function(theNodeMap) {
     }
 };
 
-// games and players
+// tie user information to in-game player information
 Players = new Meteor.Collection("players");
+Players.allow({ // allow user client-side CRUD of self
+    insert: function (userId, playerObj) {
+        return true;
+    },
+    update: function (userId, playerObj) {
+        return Meteor.userId() === userId;
+    },
+    remove: function (userId, playerObj) {
+        return Meteor.userId() === userId;
+    }
+});
+createStdCollectionLink("players", Players);
