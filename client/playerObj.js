3f2009bb5338e8e0ce97f522683b135727562a81
Jason Zerbe <jzerbe@vraidsys.com>
2013-11-17 15:45:16 -0700

    add neighbor node loading

--- client/playerObj.js
+++ client/playerObj.js
@@ -1,43 +1,89 @@
-function PlayerObj(userName) {
-    var private_UserName = userName;
-    var private_GameId = "";
-    var private_IntraGamePlayerNumber = -1; // player 0 is Mr. X
-    var private_NodeId = "";
+function PlayerObj(userId, userName) {
+    var _userId = userId;
+    var _userName = userName;
+    var _gameId = "";
+    var _playerNumber = -1; // player 0 is Mr. X
+    var _nodeId = "0"; // 1 - 199
+    var _nodeNeighbors = {'underground':null, 'bus':null, 'taxi':null, 'black':null};
 
     function _getDomId(playerNumber) {
         return "#player_" + playerNumber.toString();
     }
 
     this.drawToBoard = function() {
-        var boardPosition = Positions.findOne({node: this.getNodeId().toString()}).fetch();
+        var boardPosition = Positions.findOne({node: this.getNodeId()});
         var domId = _getDomId(this.getPlayerNumber());
         $(domId).css("display", "");
         $(domId).css("left", boardPosition.x);
         $(domId).css("top", boardPosition.y);
     };
 
+    this.loadNodeNeighbors = function() {
+        var nodeUnderground = Underground.findOne({node: this.getNodeId()});
+        if (nodeUnderground) {
+            _nodeNeighbors.underground = nodeUnderground.neighbors;
+        } else {
+            _nodeNeighbors.underground = null;
+        }
+
+        var nodeBus = Bus.findOne({node: this.getNodeId()});
+        if (nodeBus) {
+            _nodeNeighbors.bus = nodeBus.neighbors;
+        } else {
+            _nodeNeighbors.bus = null;
+        }
+
+        var nodeTaxi = Taxi.findOne({node: this.getNodeId()});
+        if (nodeTaxi) {
+            _nodeNeighbors.taxi = nodeTaxi.neighbors;
+        } else {
+            _nodeNeighbors.taxi = null;
+        }
+
+        _nodeNeighbors.black = null;
+        if (this.isMrX()) {
+            var nodeBlack = Black.findOne({node: this.getNodeId()});
+            if (nodeBlack) {
+                _nodeNeighbors.black = nodeBlack.neighbors;
+            }
+        }
+    };
+
+    this.isMrX = function() {
+        return _playerNumber === 0;
+    };
+
     this.getGameId = function() {
-        return private_GameId;
+        return _gameId;
     };
     this.setGameId = function(gameId) {
-        private_GameId = gameId;
+        _gameId = gameId;
     };
 
     this.getNodeId = function() {
-        return private_NodeId;
+        return _nodeId;
     };
     this.setNodeId = function(nodeId) {
-        private_NodeId = nodeId;
+        if (typeof nodeId === 'number' || nodeId instanceof Number) {
+            _nodeId = nodeId.toString();
+        } else if (typeof nodeId === 'string' || nodeId instanceof String) {
+            _nodeId = nodeId;
+        } else {
+            throw "PlayerObj.setNodeId accepts Numbers and will begrudgingly handle Strings only";
+        }
     };
 
     this.getPlayerNumber = function() {
-        return private_IntraGamePlayerNumber;
+        return _playerNumber;
     };
     this.setPlayerNumber = function(playerNumber) {
-        private_IntraGamePlayerNumber = playerNumber;
+        _playerNumber = playerNumber;
     };
 
+    this.getUserId = function () {
+        return _userId;
+    };
     this.getUserName = function() {
-        return private_UserName;
+        return _userName;
     };
 }
