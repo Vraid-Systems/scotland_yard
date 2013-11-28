49d6509a3f746f08ef39b213faf58ad312fc0f61
Jason Zerbe <jason.zerbe@webfilings.com>
2013-11-28 15:39:22 -0600

    add new secret generate note

--- server/login_service_config.js
+++ server/login_service_config.js
@@ -5,4 +5,5 @@ Accounts.loginServiceConfiguration.insert({
     service: "facebook",
     appId: "236998419797845",
     secret: "73b6366c5b76f2ebb21b581459c7c7f2"
+    //TODO generate new FB secret when out of sandbox mode
 });
